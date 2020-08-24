ce4b63b00cb6584917e82eb69750b3a14fd7d65b
preeti <preetip10.mishra@gmail.com>
2020-08-24 16:33:21 +1000

    to do app list items finished

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+
+import './App.css';
+
+class App extends React.Component {
+  
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Yoga & Pranayam',
+        completted: false
+      },
+      {
+        id: 2,
+        title: 'Learn javaScript',
+        completted: true
+      },
+      {
+        id: 3,
+        title: 'Cooking & cleaning',
+        completted: true
+      },
+      {
+        id: 4,
+        title: 'Job Application',
+        completted: false
+      }
+    ]
+  }
+
+      
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completted = !todo.completted
+      }
+      return todo;
+    }) });
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => 
+      todo.id !== id
+    )] });
+  }
+
+  render() {
+    return (
+      <div style={mainContainer}>
+        <Header />
+        <Todos 
+          todos={this.state.todos} 
+          markComplete={this.markComplete} 
+          delTodo={this.delTodo}
+          />
+      </div>
+    );
+  }
+ 
+}
+
+const mainContainer= {
+  marginRight: '20%', marginLeft: '20%'
+}
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+//import './App.css';
+
+class Todos extends React.Component {
+
+
+    render() {
+       // console.log(this.props.todos);
+       return this.props.todos.map((todo) => (
+            <TodoItem 
+            key={todo.id} 
+            todo={todo}
+            markComplete={this.props.markComplete} 
+            delTodo={this.props.delTodo}
+            />
+       ));
+ 
+    }
+    
+}
+
+
+//PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+
+
+export default Todos;
